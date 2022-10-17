0d9d07d678241b7423ebfc2efd462275a3b4e693
Luis <lmr.87@protonmail.ch>
2022-10-18 01:16:38 +0200

    kjh

--- src/hilos/Saludo.java
+++ src/hilos/Saludo.java
@@ -1,2 +1,10 @@
-package hilos;public class Saludo {
+package hilos;
+// implemento la interfaz Runnable
+public class Saludo implements Runnable {
+
+    // Es necesario sobreescribir el metodo run con el comportamiento deseado
+    @Override
+    public void run() {
+        System.out.println("Hilo creado desde la interfaz Runnable!!");
+    }
 }
--- src/main/MainSaludo.java
+++ src/main/MainSaludo.java
@@ -1,2 +1,15 @@
-package main;public class MainSaludo {
+package main;
+
+import hilos.Saludo;
+
+public class MainSaludo {
+    public static void main(String args[]) {
+        // Creo un objeto del tipo Saludo
+        Saludo miRunnable = new Saludo();
+        // Creación de un objeto Thread (hilo (thread) hilo1) pasando como argumento al constructor el objeto Saludo creado
+        Thread hilo1 = new Thread(miRunnable);
+        // Invocamos el metodo start() del hilo1
+        System.out.println("Creando un nuevo hilo desde el hilo principal (main).");
+        hilo1.start();
+    }
 }
